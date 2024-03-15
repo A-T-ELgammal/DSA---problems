1a0349f2b0fccf2140677f33380b8c9bbf2a5123
Ahmed Elgammal <a.t.elgammal@gmail.com>
2024-03-15 17:37:24 +0200

    time & space complexity

--- recursion/sum_of_first_N_numbers.h
+++ recursion/sum_of_first_N_numbers.h
@@ -1,3 +1,4 @@
+// O(n) space-O(n)
 int sumOfFirstNaturalNumberRecursion(int n)
 {
     if (n == 0)
@@ -5,7 +6,7 @@ int sumOfFirstNaturalNumberRecursion(int n)
     else
         return sumOfFirstNaturalNumberRecursion(n - 1) + n;
 }
-
+// O(n) space-O(1)
 int sumOfFirstNaturalNumberLoop(int n)
 {
     int sum = 0;
@@ -13,7 +14,7 @@ int sumOfFirstNaturalNumberLoop(int n)
         sum += i;
     return sum;
 }
-
+// O(1) space-O(1)
 int sumOfFirstNaturalNumberEquation(int n)
 {
     return n * (n + 1) / 2;
